20acfa706407ea5baf027bd25e641a6c47955285
Abhinav Arya <itzabhinavarya@gmail.com>
2023-01-04 17:52:36 +0530

    okh h sb

---
+++ index.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(){
+//     var i = $("img");
+//     i.animate({top:'5px',bottom:'5px'},"slow");
+// });
+
+// $(document).ready(function() {
+//     setInterval(function() {
+//         $('#animate').slideUp('5000', function() {
+//             $('#animate').slideDown('5000');
+//         });
+//     }, 1500);
+// });
+function loop() {
+    $('#animate').animate({'top': '525'}, {
+        duration: 800, 
+        complete: function() {
+            $('#animate').animate({top: 535}, {
+                duration: 800, 
+                complete: loop});
+        }});
+}
+loop();
+
+function newloop() {
+    $('#laptop').animate({'left': '2'}, {
+        duration: 1000, 
+        complete: function() {
+            $('#laptop').animate({left: -2}, {
+                duration: 1000, 
+                complete: newloop});
+        }});
+}
+newloop();
+
