f7dc24e3db6697f62e16e7e9a2cba5a31364696a
Abhinav Arya <itzabhinavarya@gmail.com>
2023-01-10 17:48:21 +0530

    New Styling

--- index.js
+++ index.js
@@ -33,3 +33,18 @@ window.onload = (event) => {
     let bsAlert = new bootstrap.Toast(myAlert);
     bsAlert.show();
 }
+
+const text = document.querySelector(".second-text");
+const textLoad = () =>{
+    setTimeout(()=>{
+        text.textContent = "Developer";
+    },0);
+    setTimeout(()=>{
+        text.textContent = "Frelancer";
+    },4000);
+    setTimeout(()=>{
+        text.textContent = "Designer";
+    },8000);
+}
+textLoad();
+setInterval(textLoad,12000)
